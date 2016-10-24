773914f694cdac566fd480d9e27ba575944743ba
fedorovalex <INOY310@yandex.ru>
2016-10-24 23:18:50 +0300

    Добавлены описания Javadoc.

--- src/main/java/com/epam/task01/Solution.java
+++ src/main/java/com/epam/task01/Solution.java
@@ -5,22 +5,27 @@ import java.util.ArrayList;
 
 import static java.lang.Math.abs;
 
+/**
+ * Решение первого задания.
+ */
 public class Solution {
 
     /**
+     * Возвращает количество заданных символов в строке.
      *
-     * @param line
-     * @param symbol
-     * @return
+     * @param line - строка, в которой происход поиск.
+     * @param symbol - заданный символ.
+     * @return - Возвращает количество заданных символов.
      */
     public int getAmountSymbol(String line, String symbol) {
         return line.length() - line.replace(symbol, "").length();
     }
 
     /**
+     * Определяет, что в массиве строк есть одинаковые элементы.
      *
-     * @param lines
-     * @return
+     * @param lines - массив строк.
+     * @return - Возвращает true если одинаковые элементы найдены, иначе false.
      */
     public boolean isRepeatLineInArray(String[] lines) {
 
@@ -39,10 +44,11 @@ public class Solution {
     }
 
     /**
+     * Возвращает перемножение двух матриц.
      *
-     * @param firstMatrix
-     * @param secondMatrix
-     * @return
+     * @param firstMatrix - первая матрица.
+     * @param secondMatrix - вторая матрица.
+     * @return - Возвращает произведение двух матриц, если оно существует, иначе null.
      */
     public int[][] getMatrixMultiplication(int[][] firstMatrix, int[][] secondMatrix) {
 
@@ -73,11 +79,12 @@ public class Solution {
 
 
     /**
+     * Возвращает пересечение массивов double.
      *
-     * @param first
-     * @param second
-     * @param epsilon
-     * @return
+     * @param first - первый массив.
+     * @param second - второй массив.
+     * @param epsilon - погрешность сравнения double.
+     * @return - Возвращает массив double с пересечение входных массивов.
      */
     public ArrayList<Double> getArraysIntersection(double[] first, double[] second, double epsilon) {
         ArrayList<Double> intersection = new ArrayList<Double>();
@@ -95,15 +102,16 @@ public class Solution {
 
 
     /**
+     * Проверяет матрицу на пригодность.
+     * Матрица пригодна если она не null и имеет одинаковое количество элементов в каждой строке.
      *
-     * @param matrix
-     * @return
+     * @param matrix - проверяемая матрица.
+     * @return - Возвращает true если матрица пригодна, иначе false.
      */
     private boolean isMatrixValid(int[][] matrix) {
 
         boolean isValid = true;
         int columnCount = 0;
-
 
         if (matrix == null || matrix.length == 0 || matrix[0] == null) {
             isValid = false;
